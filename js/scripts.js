0f83433e11a21d6b9c61a3d302dc1195744a83b6
Jamalperry <lee.perry400@gmail.com>
2019-09-10 18:42:21 -0400

    place more jquery toggle comments with corresponding css and texts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-	$(".mathen").click(function() {
-		$(".matcom").toggle(1000);
-		$(".matshow").toggle(1000);
-		$(".mathide").toggle(1000);
+	$(".mathen1").click(function() {
+		$(".matcom1").toggle(1000);
+		$(".matshow1").toggle(1000);
+		$(".mathide1").toggle(1000);
+	});
+	
+	$(".mathen2").click(function() {
+		$(".matcom2").toggle(1000);
+		$(".matshow2").toggle(1000);
+		$(".mathide2").toggle(1000);
+	});
+	$(".mathen3").click(function() {
+		$(".matcom3").toggle(1000);
+		$(".matshow3").toggle(1000);
+		$(".mathide3").toggle(1000);
 	});
 /*
 $(".dake").click(function() {
