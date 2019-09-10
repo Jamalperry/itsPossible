5be4a796986ccce21eca21fd1c646388b2c9ef62
Jamalperry <lee.perry400@gmail.com>
2019-09-10 12:58:39 -0400

    add more texts css edits js and jquery edits

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".mathen").click(function() {
+		$(".matcom").toggle(1000);
+		$(".matshow").toggle(1000);
+		$(".mathide").toggle(1000);
+	});
+/*
+$(".dake").click(function() {
+		$(".dakecom").toggle();
+		$(".dakeshow").toggle();
+		$(."dakehide").toggle();
+	});*/
+});
