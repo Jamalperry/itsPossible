86a9c6e41b244896d092c4dc6e4ccd28e5c3d54a
Jamalperry <lee.perry400@gmail.com>
2019-09-11 11:27:50 -0400

    add more jquery toggles, a border round img circles and text to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
 	$(".mathen1").click(function() {
-		$(".matcom1").toggle(1000);
-		$(".matshow1").toggle(1000);
-		$(".mathide1").toggle(1000);
+		$(".matcom1").toggle("slow");
+		$(".matshow1").toggle("slow");
+		$(".mathide1").toggle("slow");
 	});
-	
 	$(".mathen2").click(function() {
-		$(".matcom2").toggle(1000);
-		$(".matshow2").toggle(1000);
-		$(".mathide2").toggle(1000);
+		$(".matcom2").slideToggle(1000);
+		$(".matshow2").slideToggle(1000);
+		$(".mathide2").slideToggle(1000);
 	});
 	$(".mathen3").click(function() {
-		$(".matcom3").toggle(1000);
-		$(".matshow3").toggle(1000);
-		$(".mathide3").toggle(1000);
+		$(".matcom3").slideToggle(1000);
+		$(".matshow3").slideToggle(1000);
+		$(".mathide3").slideToggle(1000);
+	});
+	$(".mathen4").click(function() {
+		$(".matcom4").toggle(1000);
+		$(".matshow4").toggle(1000);
+		$(".mathide4").toggle(1000);
+	});
+	$(".mathen5").click(function() {
+		$(".matcom5").slideToggle(1000);
+		$(".matshow5").slideToggle(1000);
+		$(".mathide5").slideToggle(1000);
 	});
 /*
 $(".dake").click(function() {
