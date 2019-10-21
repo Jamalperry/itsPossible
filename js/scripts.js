ba67cb830a8f2c2165e135840016b7ae4d35e1db
Jamalperry <lee.perry400@gmail.com>
2019-10-21 14:47:24 -0400

    add an intro page more text, images and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	$(".btn-enter").click(function() {
+		$(".entrance").fadeToggle(3000, function() {
+		$(".main").slideToggle(3000);
+		});
+	});
+	
+	$(".btn-danger").click(function() {
+		$(".entrance").fadeToggle(3000, function() {
+		$(".badmain").slideToggle(3000);
+		});
+	});
+	
+	
+	
 	$(".mathen1").click(function() {
 		$(".matcom1").toggle("slow");
 		$(".matshow1").toggle("slow");
@@ -23,6 +37,11 @@ $(document).ready(function() {
 		$(".matcom5").slideToggle(1000);
 		$(".matshow5").slideToggle(1000);
 		$(".mathide5").slideToggle(1000);
+	});
+	$(".mathen6").click(function() {
+		$(".matcom6").slideToggle(1000);
+		$(".matshow6").slideToggle(1000);
+		$(".mathide6").slideToggle(1000);
 	});
 /*
 $(".dake").click(function() {
